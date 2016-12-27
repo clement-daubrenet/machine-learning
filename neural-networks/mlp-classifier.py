fb619ab288a1524d5c795f039a12593b00a5454c
home <clement.daubrenet@gmail.com>
2016-12-27 20:29:51 +0000

    MLP class

--- neural-networks/mlp-classifier.py
+++ neural-networks/mlp-classifier.py
@@ -3,20 +3,37 @@ from sklearn.neural_network import MLPClassifier
 # Just playing with a MLP classifier (multi-layer perception)
 # http://scikit-learn.org/stable/modules/neural_networks_supervised.html
 
-# TODO : transform into a clear function including a proper description
-# of the solver, alpha , hidden layers terms
+# TODO: start a classifier MLP classifier and compare to scikit
 
-# X, training samples vector
-training_set = [[0., 0.], [1., 1.]]
+class MLP:
 
-# Y target values for the training samples
-target = [0, 1]
+    def __init__(self):
+        self.clf = MLPClassifier(solver="lbfgs",
+                                 alpha=1e-5,
+                                 hidden_layer_sizes=(5, 2),
+                                 random_state=1)
 
-# MLP classifier
-clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=1)
+    def fit_with_mlp(self, sample, target):
+        """
+        Fiting using MLP classifier
+        :param sample: input training sample
+        e.g: sample = [[0., 0.], [1., 1.]]
+        :param target: target for the given training sample
+        e.g: output [0, 1]
+        """
+        self.clf.fit(sample, target)
 
-# We fit the model on the training data
-clf.fit(training_set, target)
+    def predict_with_mlp(self, new_sample):
+        """
+        Predicting using MLP classifier
+        :param new_sample: new sample to use for prediction
+        e.g: new_sample = [[1., 0.], [0., 1.]]
+        :return:
+        """
+        self.clf.predict(new_sample)
 
-# We predict the label on new samples
-print(clf.predict(([[2., 2.], [-1., -2.]])))
+
+if __name__ == "__main__":
+    mlp_classifier = MLP()
+    mlp_classifier.fit_with_mlp([[0., 0.], [1., 1.]], [0., 1.])
+    mlp_classifier.predict_with_mlp([[1., 0.], [0., 1.]])
