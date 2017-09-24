1ae0b65372d82b19e43c29969d114f8ab14836d5
clement <clement.daubrenet@gmail.com>
2017-09-24 21:13:58 +0100

    Travelling salesman, city class

---
+++ travelling_salesman/cities.py
@@ -0,0 +1,11 @@
+class City(object):
+    """
+    City element, discribing its position on a map.
+    """
+    def __init__(self, x, y):
+        """
+        :param x: x position on the grid.
+        :param y: y position on the grid.
+        """
+        self.x = x
+        self.y = y
