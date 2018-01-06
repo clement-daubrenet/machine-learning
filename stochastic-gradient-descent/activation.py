98a200e62647efca14b466dbf2cd5ba5a43ee043
clement-daubrenet <clement.daubrenet@gmail.com>
2018-01-06 08:49:42 +0000

    Update

--- stochastic-gradient-descent/activation.py
+++ stochastic-gradient-descent/activation.py
@@ -1,7 +1,9 @@
 def activation(bias, weights, inputs):
     """
-    Activation function for Perceptron.
+    Activation function for Perceptron. 
+    Its a step function: if the weigted sum is positive, returns 1. If not, returns 0.
     A good article here: https://medium.com/the-theory-of-everything/understanding-activation-functions-in-neural-networks-9491262884e0
+    An application there: https://machinelearningmastery.com/implement-perceptron-algorithm-scratch-python/
     :param bias: the bias value often named "Beta0" in articles.
     :param weights: the list of weignts for the weighted sum.
     :param inputs: inputs from the training set.
@@ -9,4 +11,6 @@ def activation(bias, weights, inputs):
     activation += bias
     for index, weight in enumerate(weights):
         activation += weigh * inputs[index]
-    return activation
+    if activation>=0:
+        return 1
+    return 0 
