56c47f7dff9e26f44835b46980395b4a0425221d
clement-daubrenet <clement.daubrenet@gmail.com>
2018-04-16 21:10:29 +0100

    Initialization games function

--- taming/taming-1.py
+++ taming/taming-1.py
@@ -10,11 +10,16 @@ from collections import Counter
 LR = 1e-3
 env = gym.make('CartPole-v0')
 env.reset()
-goal_steps = 1000
+goal_steps = 500
 score_requirement = 50
 initial_games = 10000
 
+
 def some_random_games_first():
+    """
+    Just playing 50 random games.
+    :return:
+    """
     for episode in range(50):
         env.reset()
         for t in range(goal_steps):
@@ -24,4 +29,49 @@ def some_random_games_first():
             if done:
                 break
 
-some_random_games_first()
+
+def initial_population():
+    """
+    :return:
+    """
+    training_data = []
+    scores = []
+    accepted_scores = []
+    for _ in range(initial_games):
+        score = 0
+        game_memory = []
+        prev_observation = []
+        for _ in range(goal_steps):
+            action = random.randrange(0, 2)
+            observation, reward, done, info = env.step(action)
+          
+            if len(prev_observation) > 0:
+                game_memory.append([prev_observation, action])
+            prev_observation = observation
+            score += reward
+            if done:
+                break
+      
+        if score >= score_requirement:
+            accepted_scores.append(score)
+            for data in game_memory:
+                if data[1] == 1:
+                    output = [0, 1]
+                elif data[1] == 0:
+                    output = [1, 0]
+                training_data.append([data[0], output])
+
+        env.reset()
+        scores.append(score)
+      
+    training_data_save = np.array(training_data)
+    np.save('saved.npy', training_data_save)
+
+    print('Average accepted score:', mean(accepted_scores))
+    print('Median:', median(accepted_scores))
+    print(Counter(accepted_scores))
+
+    return training_data
+
+
+initial_population()
